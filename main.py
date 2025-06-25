b83d70ebb3f07e6ca8d0008c38c20ab5b05088b5
markiu2007@interia.pl <markiu2007@interia.pl>
2025-06-25 15:55:06 +0200

    Add HTML reading thing that makes <body> part into string

--- main.py
+++ main.py
@@ -1,4 +1,84 @@
 import os
+from enum import Enum
+
+
+class FileType(Enum):
+    html = 1
+    xml = 2
+    css = 3
+
+
+class WebpageFile:
+    def __init__(self, path: str, file_type: FileType):
+        self.path = path
+        self.file_type = file_type
+    
+    def create_div(self) -> str:
+        if self.file_type == FileType.html:
+            return self.__create_div_from_html()
+        else:
+            raise Exception
+
+    def __create_div_from_html(self) -> str:
+        """For HTML files, only return contents of \<body\> as is."""
+        result = ""
+        save_flag = False
+        with open(self.path) as f:
+            for line in f:
+                if "<body>" in line:
+                    save_flag = True
+                    continue
+                if "</body>" in line:
+                    break
+                if save_flag:
+                    result += line
+        result = f"<div>{result}</div>"
+        return result
+
+
+class Section:
+    def __init__(self, webpage_files: list[WebpageFile]):
+        self.webpage_files = webpage_files
+
+
+class NavigationItem:
+    def __init__(self, title: str, section: list[Section], parent: Section):
+        self.title = title
+        self.section = section
+        self.parent = parent
+
+
+class WebpageBuilder:
+    def __init__(self, webpage_path):
+        self.webpage_path = webpage_path
+        self.directory_structure = self.get_directory_structure(self.webpage_path)
+
+    def get_directory_structure(self, path: str) -> list:
+        local_array = []
+        for file in os.listdir(path):
+            full_path = os.path.join(path, file)
+            if os.path.isfile(full_path):
+                local_array.append(full_path)
+            else:
+                local_array.append(self.get_directory_structure(full_path))
+        return local_array
+
+    def create_navigation_structure(self):
+        pass
+
+
+webpage_builder = WebpageBuilder(os.path.join(os.getcwd(), "webpage"))
+
+test_webpage_file = WebpageFile("D:\\Dane_Gits\\HLAG\\webpage\\020_home\\010_home.html", FileType.html)
+print(test_webpage_file.create_div())
+
+test_section = Section([
+    WebpageFile("D:\\Dane_Gits\\HLAG\\webpage\\020_home\\010_home.html", FileType.html),
+    WebpageFile("D:\\Dane_Gits\\HLAG\\webpage\\020_home\\015_some_appendix.html", FileType.html),
+])
+
+
+
 
 
 
@@ -7,26 +87,26 @@ import os
 current_location = os.getcwd()
 webpage_location = os.path.join(current_location, "webpage")
 
-for (dirpath, dirnames, filenames) in os.walk(webpage_location):
-    print(dirpath, dirnames, filenames)
+# for (dirpath, dirnames, filenames) in os.walk(webpage_location):
+#     print(dirpath, dirnames, filenames)
 
-print(webpage_location)
-print(os.path.isfile(os.path.join(webpage_location, "010_start.txt")))
+# print(webpage_location)
+# print(os.path.isfile(os.path.join(webpage_location, "010_start.txt")))
 
 def go_through(path):
     local_array = []
     for file in os.listdir(path):
         full_path = os.path.join(path, file)
         if os.path.isfile(full_path):
-            print(full_path)
+            # print(full_path)
             local_array.append(full_path)
         else:
-            print(full_path + " --> ")
+            # print(full_path + " --> ")
             local_array.append(go_through(full_path))
     return local_array
 
 file_array = go_through(webpage_location)
-print(file_array)
+# print(file_array)
 
 
 
