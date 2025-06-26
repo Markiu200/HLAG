a8694448d5d3d470c96a9605e3dfe85d13003a9b
markiu2007@interia.pl <markiu2007@interia.pl>
2025-06-26 02:09:34 +0200

    Implement navigation js generation

--- main.py
+++ main.py
@@ -71,9 +71,34 @@ class Navigation:
         for section in self.section_list:
             if section.id == "root":
                 continue
-            result += f"<button>{section.id}</button>"
+            result += f"<button onclick=\"navigation.show('{section.id}')\">{section.id}</button>"
         result += "</nav>\r\n"
         return result
+    
+    def get_navigation_js_as_string(self) -> str:
+        # get sections map
+        js_map = ""
+        for section in self.section_list:
+            if section.id == "root":
+                continue
+            js_map += f"[\"{section.id}\"], document.getElementById(\"{section.id}\")],\r\n"
+        # generate result
+        result = """<script>
+  const navigation = {
+    sections: new Map([
+      """ + js_map + """
+    ]),
+    show(section) {
+      this.sections.forEach((sec) => {
+        sec.style.display = "none";
+        if (sec.id == section) {
+          sec.style.display = "block";
+        }
+      });
+    }
+  };
+</script>"""
+        return result        
 
 
 class FileStructureReader:
