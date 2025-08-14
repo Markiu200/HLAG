7945c953d83eb49d5f9dec779eba11446c8213c9
markiu200 <markiu2007@interia.pl>
2025-08-14 18:14:48 +0200

    Put navigation module straight in mylib

--- mylib/navigation/navigation_item.py
+++
@@ -1,6 +0,0 @@
-class NavigationItem:
-    def __init__(self, title: str, section: list[Section], parent: Section):
-        self.title = title
-        self.section = section
-        self.parent = parent
-        self.children = None
