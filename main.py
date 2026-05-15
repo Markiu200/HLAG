bc694d23e195d15e2c4087fc0feb82db9a55b76f
markiu200 <markiu2007@interia.pl>
2026-05-15 15:48:58 +0200

    Add logging module
    As well as update one .lnk.

--- main.py
+++ main.py
@@ -1,38 +1,45 @@
-from pathlib import Path
-import gui
-from models.prog_args import ProgArgs
-from builder import SiteBuilder
-
-import sys, os
-import importlib
-
-modules_lib_directory = str(Path(Path.cwd(), "modules"))
-sys.path.append(modules_lib_directory)
-print(modules_lib_directory)
-
-modules_directory_list = [f.name for f in os.scandir(modules_lib_directory) if f.is_dir() and not f.name.startswith("_")]
-modules_lib = []
-
-for module in modules_directory_list:
-    modules_lib.append((module, importlib.import_module(f"{module}.main", modules_lib_directory)))
-
-# simple = importlib.import_module("fancy_article.fancy_world", modules_lib_directory)
-# simple.fancy_article()
-
-for module in modules_lib:
-    module[1].main()
-
+# Built-in imports
 # import argparse
+import sys
+import logging
+from pathlib import Path
+
+# Own imports
+import gui
+import lib.module_manager
+from models.prog_args import ProgArgs
 
 
-# todo Launch part - check for arguments
+# Start basic logger
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s %(levelname)s %(message)s',
+    handlers=[
+        logging.FileHandler("logs.log"),
+        logging.StreamHandler(sys.stdout)
+    ]
+)
+
+# Start module manager
+modules_lib_directory = str(Path(Path.cwd(), "modules"))
+mmanager = lib.module_manager.ModuleManager(modules_lib_directory)
+
+
+# todo Launch part - make it launchable w/o GUI
 # if gui is enabled - then stop reading for other arguments and take them from GUI
 # else read all the args and assess defaults
 config: ProgArgs = gui.start()
 if not Path(config.target_path).exists():
     raise FileExistsError("File or directory of targeted path not found")
 
-# todo Invoke program
+# todo Read structure
+# ignore oos_ nodes
+# folders are in navigator
+#   Unless there's only one folder or none at all
+
+
+
 print("And so on...")
 # site_builder = SiteBuilder(root_directory_path=Path(config.target_path), images_as_base64=config.embed_images)
 
