cd6e566b718c3877c6af9141b1132ed6322ed582
markiu200 <markiu2007@interia.pl>
2026-05-26 18:55:09 +0200

    Make structure_reader be initialized in it's module
    Attempt to fix class variables

--- lib/structure_reader_new.py
+++ lib/structure_reader_new.py
@@ -1,24 +1,47 @@
 import os
-from itertools import chain
+from pathlib import PurePath
 # Own imports
 from models.config import Config
-from lib.base_outline_manager import BaseOutlineManager
+from base_outline_manager import BaseOutlineManager
 
 
 class StructureReader:
-    def __init__(self, config: Config, outline_manager: BaseOutlineManager):
+    prefix = 0
+
+    @classmethod
+    def set_prefix(cls, length):
+        cls.prefix = length
+
+    # def __init__(self, config: Config, outline_manager: BaseOutlineManager):
+    #     self.config = config
+    #     self.outline_manager = outline_manager
+
+    def __init__(self):
+        self.config = None
+        self.outline_manager = None
+
+    def set_config(self, config: Config):
         self.config = config
+
+    def set_outline_manager(self, outline_manager: BaseOutlineManager):
         self.outline_manager = outline_manager
 
     def scan(self):
-        self.scan_for_outline()
+        self._scan(self.config.target_path)
 
-    def scan_for_outline(self) -> None:
-        for path, dirs, files in os.walk(self.config.target_path):
-            pass
-            # for name in dirs:
-            #     if not name.startswith(".") and not name.startswith("_"):
-            #         self.outline_manager.register(path, name)
-            # for name in files:
-            #     if not name.startswith(".") and not name.startswith("_"):
-            #         self.outline_manager.register(path, name)
+    def _scan(self, full_path):
+        for path, dirs, files in os.walk(full_path):
+            for a_dir in dirs:
+                current_full_path = PurePath(path, a_dir)
+                self._for_outline(current_full_path)
+            for a_file in files:
+                current_full_path = PurePath(path, a_file)
+                self._for_outline(current_full_path)
+
+    def _for_outline(self, full_path: PurePath) -> None:
+        if not full_path.name.startswith(("_", ".")):
+            self.outline_manager.register(full_path)
+
+
+if 'structure_reader' not in locals():
+    structure_reader = StructureReader()
