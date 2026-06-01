ac73907180742643c8fee5a70272ed1ffaf753e0
markiu200 <markiu2007@interia.pl>
2026-06-01 20:39:03 +0200

    Update DocumentNode to take NodeAttribute as attributes

--- lib/structure_scanner/document_tree/document_node.py
+++ lib/structure_scanner/document_tree/document_node.py
@@ -1,9 +1,10 @@
 from pathlib import PurePath
+from data.node_attributes import NodeAttribute
 
 
 class DocumentNode:
     def __init__(self, path: PurePath):
-        self.children = []
+        self.children: list['DocumentNode'] = []
         self.parent = None
         #
         self.path = path
@@ -23,16 +24,16 @@ class DocumentNode:
     def get_children(self):
         return self.children
 
-    def add_attribute(self, attribute: str):
+    def add_attribute(self, attribute: NodeAttribute):
         self.attributes.add(attribute)
 
-    def get_attributes(self):
+    def get_attributes(self) -> set:
         return self.attributes
 
-    def set_metadata(self, metadata_tuple: (str,str)):
+    def set_metadata(self, metadata_tuple: (str, str)):
         self.metadata[metadata_tuple[0]] = metadata_tuple[1]
 
-    def get_metadata(self, key):
+    def get_metadata(self, key) -> str | None:
         if key in self.metadata:
             return self.metadata[key]
         return None
