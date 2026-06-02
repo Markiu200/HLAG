06823ffbb56983db28c0c5b7740846d4ac19f8c3
markiu200 <markiu2007@interia.pl>
2026-06-02 22:47:10 +0200

    Update StructureScanner test to be more descriptive

--- tests/test_structure_scanner.py
+++ tests/test_structure_scanner.py
@@ -6,7 +6,8 @@ sys.path.append("D:\\hlag\\lib")
 sys.path.append("D:\\hlag\\models")
 # Own imports
 from structure_scanner.structure_scanner import StructureScanner
-from data.node_attributes import NodeAttribute
+from data.node_attribute import NodeAttribute
+from data.node_type import NodeMetadataKey, NodeMetadataTypeValue
 from structure_scanner.document_tree.document_node import DocumentNode
 from structure_scanner.document_tree.document_tree import DocumentTree
 
@@ -16,56 +17,66 @@ class TestStructureScanner(unittest.TestCase):
         # make actual scan
         self.structure_scanner = StructureScanner(PurePath("D:\\hlag\\tests\\test_structure_for_structure_scanner"))
         self.structure_scanner.scan()
+        # print(self.structure_scanner)
 
         # create reference tree
         self.reference = DocumentTree(DocumentNode(PurePath("D:\\hlag\\tests\\test_structure_for_structure_scanner")))
         parent_item = self.reference.root
-        parent_item.set_metadata(("title", "Start")) # set in _title.txt metafile
-        parent_item.add_attribute(NodeAttribute.IN_OUTLINE)
+        parent_item.set_metadata((NodeMetadataKey.TITLE, "Start"))  # set in _title.txt metafile
+        parent_item.set_metadata((NodeMetadataKey.TYPE, NodeMetadataTypeValue.CONTAINER))  # not in file - needs to be added by scanner
+        parent_item.add_attribute(NodeAttribute.IN_OUTLINE)  # root always in outline
 
         new_item_1 = DocumentNode(PurePath("D:\\hlag\\tests\\test_structure_for_structure_scanner\\_dict.txt"))
-        new_item_1.set_metadata(("type", "dictionary"))
-        new_item_1.add_attribute(NodeAttribute.IS_ESCAPED)
+        new_item_1.set_metadata((NodeMetadataKey.TYPE, NodeMetadataTypeValue.DICTIONARY))  # set in file
+        new_item_1.add_attribute(NodeAttribute.IS_ESCAPED)  # escape character
         parent_item.add_child(new_item_1)
 
         new_item_2 = DocumentNode(PurePath("D:\\hlag\\tests\\test_structure_for_structure_scanner\\_title.txt"))
-        new_item_2.set_metadata(("title", "Start"))
-        new_item_2.set_metadata(("type", "metafile")) # not in file - needs to be added by scanner
-        new_item_2.add_attribute(NodeAttribute.IS_ESCAPED)
+        new_item_2.set_metadata((NodeMetadataKey.TITLE, "Start"))  # set in file
+        new_item_2.set_metadata((NodeMetadataKey.TYPE, NodeMetadataTypeValue.METAFILE))  # not in file - needs to be added by scanner
+        new_item_2.add_attribute(NodeAttribute.IS_ESCAPED)  # escape character
         parent_item.add_child(new_item_2)
 
         new_item_3 = DocumentNode(PurePath("D:\\hlag\\tests\\test_structure_for_structure_scanner\\000_header.txt"))
+        new_item_3.set_metadata((NodeMetadataKey.TYPE, NodeMetadataTypeValue.TEXT))  # not in file - needs to be added by scanner
         parent_item.add_child(new_item_3)
 
         new_item_4 = DocumentNode(PurePath("D:\\hlag\\tests\\test_structure_for_structure_scanner\\010_body.txt"))
+        new_item_4.set_metadata((NodeMetadataKey.TYPE, NodeMetadataTypeValue.TEXT))  # not in file - needs to be added by scanner
         parent_item.add_child(new_item_4)
 
         new_item_5 = DocumentNode(PurePath("D:\\hlag\\tests\\test_structure_for_structure_scanner\\020_image.png"))
+        new_item_5.set_metadata((NodeMetadataKey.TYPE, NodeMetadataTypeValue.IMAGE))  # needs to be added by scanner
         parent_item.add_child(new_item_5)
 
         new_item_6 = DocumentNode(PurePath("D:\\hlag\\tests\\test_structure_for_structure_scanner\\030_footer.txt"))
+        new_item_6.set_metadata((NodeMetadataKey.TYPE, NodeMetadataTypeValue.TEXT))  # not in file - needs to be added by scanner
         parent_item.add_child(new_item_6)
 
         new_item_article = DocumentNode(PurePath("D:\\hlag\\tests\\test_structure_for_structure_scanner\\100_article"))
         new_item_article.add_attribute(NodeAttribute.IN_OUTLINE)
-        new_item_article.set_metadata(("title", "Article"))
+        new_item_article.set_metadata((NodeMetadataKey.TITLE, "Article"))   # set in _title.txt metafile
+        new_item_article.set_metadata((NodeMetadataKey.TYPE, NodeMetadataTypeValue.CONTAINER))  # needs to be added by scanner
         parent_item.add_child(new_item_article)
 
         new_item_8 = DocumentNode(PurePath("D:\\hlag\\tests\\test_structure_for_structure_scanner\\100_article\\_title.txt"))
-        new_item_8.set_metadata(("title", "Article"))
-        new_item_8.set_metadata(("type", "metafile"))
+        new_item_8.set_metadata((NodeMetadataKey.TITLE, "Article"))  # set in file
+        new_item_8.set_metadata((NodeMetadataKey.TYPE, NodeMetadataTypeValue.METAFILE))  # not in file - needs to be added by scanner
         new_item_article.add_child(new_item_8)
 
         new_item_9 = DocumentNode(PurePath("D:\\hlag\\tests\\test_structure_for_structure_scanner\\100_article\\article.txt"))
-        new_item_9.set_metadata(("meta", "metadata_for_module"))
+        new_item_9.set_metadata((NodeMetadataKey.META, "metadata_for_module"))  # set in file
+        new_item_9.set_metadata((NodeMetadataKey.TYPE, NodeMetadataTypeValue.TEXT))  # not in file - needs to be added by scanner
         new_item_article.add_child(new_item_8)
 
         new_item_escaped = DocumentNode(PurePath("D:\\hlag\\tests\\test_structure_for_structure_scanner\\_110_escaped"))
         new_item_escaped.add_attribute(NodeAttribute.IS_ESCAPED)
+        new_item_escaped.set_metadata((NodeMetadataKey.TYPE, NodeMetadataTypeValue.CONTAINER))  # needs to be added by scanner
         parent_item.add_child(new_item_escaped)
 
         new_item_10 = DocumentNode(PurePath("D:\\hlag\\tests\\test_structure_for_structure_scanner\\_110_escaped\\article.txt"))
-        new_item_10.add_attribute(NodeAttribute.IS_ESCAPED)
+        new_item_10.add_attribute(NodeAttribute.IS_ESCAPED)  # not in file nor name - needs to be added by scanner (nested in escaped dir)
+        new_item_10.set_metadata((NodeMetadataKey.TYPE, NodeMetadataTypeValue.TEXT))  # not in file - needs to be added by scanner
         new_item_escaped.add_child(new_item_10)
 
         #
