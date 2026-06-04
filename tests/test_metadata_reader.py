275a08eb5d533c27cad9b6c19d8e6e4c7210389b
markiu200 <markiu2007@interia.pl>
2026-06-04 17:03:47 +0200

    Add unit tests structure scanner test

--- tests/test_metadata_reader.py
+++ tests/test_metadata_reader.py
@@ -8,13 +8,33 @@ from structure_scanner.metadata_reader.metadata_reader import get_metadata
 class TestMetadataReader(unittest.TestCase):
     def test_valid_meta(self):
         actual_meta = get_metadata(PurePath("D:\\hlag\\tests\\test_structure_for_metadata_reader\\meta1.txt"))
-        expected_meta = {
+        expected_meta = ({
             NodeMetadataKey.TITLE: "All meta types correctly",
             NodeMetadataKey.TYPE: NodeMetadataTypeValue.METAFILE,
             NodeMetadataKey.MODULE: "the_module",
             NodeMetadataKey.META: "theMeta"
-        }
-        self.assertEqual(actual_meta, expected_meta, f"Metadata different from expected.\nActual: {actual_meta}\nExpected: {expected_meta}")
+        }, False)
+        self.assertEqual(actual_meta, expected_meta,
+                         f"Metadata different from expected for valid meta test.\nActual: {actual_meta}\nExpected: {expected_meta}")
+
+    def test_incorrect_keys(self):
+        actual_meta = get_metadata(PurePath("D:\\hlag\\tests\\test_structure_for_metadata_reader\\meta2.txt"))
+        # Wrong keys are expected to be ignored (along with values)
+        expected_meta = (dict(), False)
+        self.assertEqual(actual_meta, expected_meta,
+                         f"Metadata different from expected for incorrect keys test.\nActual: {actual_meta}\nExpected: {expected_meta}")
+
+    def test_anything_else_on_new_line(self):
+        actual_meta = get_metadata(PurePath("D:\\hlag\\tests\\test_structure_for_metadata_reader\\meta3.txt"))
+        expected_meta = ({
+            NodeMetadataKey.TITLE: "Only title but leading new line and some untagged text after that"
+        }, True)
+
+    def test_anything_else_on_same_line(self):
+        actual_meta = get_metadata(PurePath("D:\\hlag\\tests\\test_structure_for_metadata_reader\\meta3a.txt"))
+        expected_meta = ({
+            NodeMetadataKey.TITLE: "Only title and some text after tag on the same line"
+        }, True)
 
 
 if __name__ == "__main__":
