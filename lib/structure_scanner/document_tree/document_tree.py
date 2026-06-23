15bab004873fcc042d68cc995767032a4ee8a061
markiu200 <markiu2007@interia.pl>
2026-06-23 17:03:30 +0200

    Add compare method for DocumentTree

--- lib/structure_scanner/document_tree/document_tree.py
+++ lib/structure_scanner/document_tree/document_tree.py
@@ -11,6 +11,21 @@ class DocumentTree:
         yield self.root
         yield from self._iter_loop(self.root)
 
+    def __eq__(self, other_tree: 'DocumentTree'):
+        own_nodes = [node for node in self]
+        other_nodes = [node for node in other_tree]
+
+        length_difference = len(own_nodes) - len(other_nodes)
+        if length_difference != 0:
+            return False
+
+        for own_node in own_nodes:
+            other_node = other_tree.get_node_by_path(own_node.path)
+            if own_node != other_node:
+                return False
+
+        return True
+
     def _iter_loop(self, node: DocumentNode):
         for child in node.children:
             yield child
