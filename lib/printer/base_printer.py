7fe9b36618de9a8b10d245b2caa1cb45253969a5
markiu200 <markiu2007@interia.pl>
2026-06-29 13:16:51 +0200

    Make BasePrinter more vague, more like interface

--- lib/printer/base_printer.py
+++ lib/printer/base_printer.py
@@ -1,18 +1,7 @@
 from abc import ABC, abstractmethod
-from pathlib import PurePath
-# Own imports
-from indent_manager import BaseIndentManager
 
 
 class BasePrinter(ABC):
-    @abstractmethod
-    def set_indent_manager(self, indent_manager: BaseIndentManager):
-        pass
-
-    @abstractmethod
-    def set_output_file_path(self, path: PurePath):
-        pass
-
     @abstractmethod
     def register(self, iterable):
         pass
