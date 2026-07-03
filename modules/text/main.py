55a2f95ba2e386c9154bf87842cc3b5bfc4a9155
markiu200 <markiu2007@interia.pl>
2026-07-03 11:43:25 +0200

    Implement basic Text module
    More of a proof of concept for all future modules, but "Text" should be most basic one.

--- modules/text/main.py
+++ modules/text/main.py
@@ -1,6 +1,13 @@
 # Own imports
 from modules.base_module import BaseModule
+from structure_scanner.document_tree.document_node import DocumentNode
 
 
 class Text(BaseModule):
-    pass
+    def __init__(self, node: DocumentNode):
+        super().__init__(node)
+
+    def print(self):
+        super().read()
+        super().replace_references()
+        yield super().write(self.content)
