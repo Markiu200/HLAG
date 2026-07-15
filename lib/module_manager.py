dadd2b8813cfa8244636e36c366f067cc4d04e87
markiu200 <markiu2007@interia.pl>
2026-07-15 21:02:40 +0200

    Update ModuleManager to be singleton and add new methods

--- lib/module_manager.py
+++ lib/module_manager.py
@@ -6,36 +6,50 @@ import logging
 logger = logging.getLogger(__name__)
 
 
+class Module:
+    def __init__(self, name: str, module):
+        self.name = name
+        self.module = module
+
+
 class ModuleManager:
-    def __init__(self, module_dir_path):
-        self.module_dir_path = ""
-        self.modules = []
+    modules_dir_path = ""
+    found_modules = []
 
-        self.set_module_dir_patch(module_dir_path)
-        self.fetch_modules()
-
-    def set_module_dir_patch(self, path):
+    @classmethod
+    def set_module_dir_patch(cls, path):
+        # todo new logger
+        path = str(path)  # neither sys not importlib do PurePaths
         if os.path.isdir(path):
             logger.info(f"Module directory found at {path}.")
-            self.module_dir_path = path
+            cls.modules_dir_path = path
             sys.path.append(path)
         else:
             logger.critical(f"User module directory {path} not found.")
             raise NotADirectoryError(f"User module directory {path} not found.")
 
-    def fetch_modules(self):
-        modules_directory_list = [f.name for f in os.scandir(self.module_dir_path) if f.is_dir() and not f.name.startswith("_")]
+    @classmethod
+    def fetch_modules(cls):
+        modules_directory_list = [f.name for f in os.scandir(cls.modules_dir_path) if f.is_dir() and not f.name.startswith("_")]
         logger.info(f"Found {len(modules_directory_list)} user modules - {modules_directory_list}")
 
         try:
             for a_module in modules_directory_list:
-                self.modules.append((a_module, importlib.import_module(f"{a_module}.main", self.module_dir_path)))
+                cls.found_modules.append(Module(
+                    name=a_module,
+                    module=importlib.import_module(f"{a_module}.main")
+                ))
         except Exception as e:
             logger.critical(f"Error occurred during user modules import.")
             raise e
 
-    def get_module_names(self):
-        return [x[0] for x in self.modules]
+    @classmethod
+    def initiate_modules(cls):
+        for module in cls.found_modules:
+            module.module.init()
 
-    def get_module_objects(self):
-        return [x[1] for x in self.modules]
+    @classmethod
+    def get_print_method(cls, module_name: str):
+        for module in cls.found_modules:
+            if module.name == module_name:
+                return module.module.parse
