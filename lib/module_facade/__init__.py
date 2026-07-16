b4adb606453edc5b126f18d9ae7d55462e2062a6
markiu200 <markiu2007@interia.pl>
2026-07-16 16:09:47 +0200

    Implement ModuleFacade
    For modules to use.

--- lib/module_facade/__init__.py
+++ lib/module_facade/__init__.py
@@ -1,1 +1,3 @@
 from .module_facade import ModuleFacade
+from .module_facade import BaseCheck
+from .module_facade import DocumentNode
--- lib/module_facade/module_facade.py
+++ lib/module_facade/module_facade.py
@@ -1,9 +1,16 @@
 from pathlib import PurePath
 # Own imports
 from js_manager import JSManager
+from structure_scanner import StructureScanner
+from structure_scanner import BaseCheck
+from structure_scanner import DocumentNode
 
 
 class ModuleFacade:
     @classmethod
     def register_js(cls, path: PurePath):
         JSManager.register(path)
+
+    @classmethod
+    def register_check(cls, callback):
+        StructureScanner.register_pre_metaread_node_check(callback)
