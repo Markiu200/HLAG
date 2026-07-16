8316568226d5631d115b590b4046c1fe860af1ed
markiu200 <markiu2007@interia.pl>
2026-07-16 16:14:04 +0200

    Fix main to acknowledge StructureScanner as a singleton kind

--- modules/text/main.py
+++ modules/text/main.py
@@ -1,13 +1,18 @@
 import re
+import sys
 from pathlib import PurePath
+sys.path.append(str(PurePath(__file__).parent))
 # Own imports
 from module_facade import ModuleFacade
+from txt_check import TXTCheck
 
 
 def init():
     print("Initiating Text module...")
     module_path = PurePath(__file__).parent
+    sys.path.append(str(PurePath(module_path, "txt_check.py")))
     ModuleFacade.register_js(PurePath(module_path, "js.js"))
+    ModuleFacade.register_check(TXTCheck())
 
 
 def parse(input_data):
