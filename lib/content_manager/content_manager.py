f87708a95657efa6fc1428710e3d431755270b6d
markiu200 <markiu2007@interia.pl>
2026-07-17 08:13:04 +0200

    Implement first version of ContentManager
    Not tested.

--- lib/content_manager/content_manager.py
+++ lib/content_manager/content_manager.py
@@ -1,15 +1,70 @@
 import json
+from structure_scanner import StructureScanner, DocumentNode
+from module_manager import ModuleManager
 
 
 class ContentManager:
-    count = 0
+    printable_elements_list: list[DocumentNode] = []
+    used_modules: set[str] = set()
+    saved_refs: dict = dict()  # refs beda jak {"nazwa": [str_refek]}
+    saved_refs_ids: dict = dict()
 
     @classmethod
-    def register_instance(cls, data) -> str:
-        table_data = [data["module"], cls.count, data["content"], data["metadata"]]
-        ref_data = [data["module"], cls.count, data["metadata"]]
-        cls.count += 1
-        json_table_data = json.dumps(table_data)
-        json_ref_data = json.dumps(ref_data)
-        print(f"Instance registered, count {cls.count}: {json_table_data}")
-        return f"[%JSREF({json_ref_data})%]"
+    def fetch_content_from_scanner(cls):
+        for node in StructureScanner.tree:
+            if node.metadata.get("module"):
+                cls.printable_elements_list.append(node)
+
+    @classmethod
+    def get_reference_from_file(cls, node: DocumentNode) -> str:  # zwraca JSREF
+        # todo get current module from metadata
+        module = ModuleManager.get_module(node.metadata.get("module"))
+        # todo then read the metadata using current Module meta-fetcher
+        found_meta = module.read_metadata_from_file(node)
+        # todo then update metadata
+        node.add_metadata(found_meta)
+        # todo then see what module it is after all
+        # todo then register that module as used
+        module = ModuleManager.get_module(node.metadata.get("module"))
+        # todo then append metadata read to the metadata
+        # assuming for now that it's been done via meta update above
+        # todo then invoke current Module parse along with DocumentNode
+        # todo then save the returned object (to be JSREFed at print)
+        # # todo if parser encounter reference, it asks this method for reference (passes the object)
+        jsref_dict = module.parse_from_file(node)
+        jsref = cls.register_instance(jsref_dict)
+        return jsref
+
+    @classmethod
+    def get_reference_from_data(cls, data: dict) -> str:
+        module = ModuleManager.get_module(data.get("module"))
+        found_meta = module.read_metadata_from_string(data.get("content"))
+        jsref_dict = module.parse_from_string(data.get("content"), found_meta)
+        jsref = cls.register_instance(jsref_dict)
+        return jsref
+
+    @classmethod
+    def register_instance(cls, data: dict) -> str:  # zwraca JSREF
+        # saved_refs = {module: {id:int, refs:list}}
+        module = data.get("module")
+        if not cls.saved_refs_ids.get(module):
+            cls.saved_refs_ids[module] = -1
+            cls.saved_refs[module] = []
+            cls.used_modules.add(module)
+        new_module_id = cls.saved_refs_ids[module] + 1
+        cls.saved_refs_ids[module] = new_module_id
+        #
+        jsref = f"[%JSREF({module},{new_module_id})%]"
+        ref = {
+            "id": new_module_id,
+            "data": data["data"],
+            "meta": data["meta"]
+        }
+        cls.saved_refs[module].append(ref)
+        #
+        print(f"Instance registered, count {new_module_id}: {ref}")
+        return jsref
+
+    @classmethod
+    def print(cls):
+        yield "".join(["let registered_modules = {", json.dumps(cls.saved_refs), "}"])
