e9e617c2440f3addab09beb3a294c8284fc4ea1c
markiu200 <markiu2007@interia.pl>
2026-07-18 14:09:33 +0200

    Fix EscapedCheck method call of parent node

--- modules/prog_dir_struct/escaped_check.py
+++ modules/prog_dir_struct/escaped_check.py
@@ -6,6 +6,6 @@ class EscapedCheck(BaseCheck):
     def check(self, node: DocumentNode):
         if node.path.name.startswith("_"):
             node.add_attribute("escaped")
-        if node.parent and node.parent.get_attribute("escaped"):
+        if node.parent and node.parent.has_attribute("escaped"):
             node.add_attribute("escaped")
         return "escaped"
