aad5199c311222d020f52c301471b4b0c77e6fa0
markiu200 <markiu2007@interia.pl>
2026-07-18 14:11:49 +0200

    Remove todo comments
    They are done.

--- modules/prog_dir_struct/dir_meta_check.py
+++ modules/prog_dir_struct/dir_meta_check.py
@@ -9,10 +9,7 @@ class DirMetaCheck(BaseCheck):
         if len(dir_node.children) > 0:
             for child in dir_node.children:
                 if os.path.isfile(child.path):
-                    # todo check if it has a proper name AND is escaped
                     if child.path.name == "_meta.txt" or child.path.name == "_metafile.txt":
-                        # todo then borrow Text module metadata reading
                         got_meta = ModuleFacade.get_module("text").read_metadata_from_file(child)
-                        # todo then apply metadata to the directory
                         dir_node.add_metadata(got_meta)
         return result
