d3e0e9972903c78dac9101fd38cd7bd430b05eca
markiu200 <markiu2007@interia.pl>
2026-07-31 16:16:46 +0200

    Remove unused class method in BaseCheck
    Access to StructureScanner can be achieved via ModuleFacade if needed.

--- lib/structure_scanner/base_check.py
+++ lib/structure_scanner/base_check.py
@@ -1,15 +1,8 @@
 from abc import ABC, abstractmethod
 from structure_scanner.document_node import DocumentNode
-# Own imports
-if __name__ == "__main__":
-    from structure_scanner.structure_scanner import StructureScanner
 
 
 class BaseCheck(ABC):
-    @classmethod
-    def set_scanner(cls, scanner: 'StructureScanner'):
-        cls.scanner = scanner
-
     @abstractmethod
     def check(self, node: DocumentNode):
         pass
