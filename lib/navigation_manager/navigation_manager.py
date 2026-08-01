cc3c38c49df089f15c43ea99b18db36f51ab0d06
markiu200 <markiu2007@interia.pl>
2026-08-01 13:29:03 +0200

    Implement prototype Navigation methods - fetch and jswindow generator
    fetch_content_from_scanner() saves internally directories that are not escaped.
    generate_jswindows outputs jswindows to be used flat in JS.

--- lib/navigation_manager/navigation_manager.py
+++ lib/navigation_manager/navigation_manager.py
@@ -1,4 +1,51 @@
+import os
+import json
+# Own imports
+from structure_scanner import StructureScanner, DocumentNode
+
+
 class NavigationManager:
+    nodes_is_outline: list[DocumentNode] = []
+    jswindows = []
+    last_jswindow_id = 0
+
+    @classmethod
+    def fetch_content_from_scanner(cls):
+        for node in StructureScanner.tree:
+            if os.path.isdir(str(node.path)) and not node.has_attribute("escaped"):
+                cls.nodes_is_outline.append(node)
+                print(f"Navigation - node found in outline: {node.path}")
+
+    @classmethod
+    def generate_jswindows(cls):
+        """Here, a flat list of jswindow is generated, meant to be placed flat
+        in the final document, outside of any JS class."""
+        for node in cls.nodes_is_outline:
+            jswindow = {
+                "id": cls.last_jswindow_id,
+                "title": f"windowid{cls.last_jswindow_id}"
+            }
+            cls.last_jswindow_id += 1
+            #
+            if node.metadata.get("title"):
+                jswindow["title"] = node.metadata.get("title")
+            #
+            contents = []
+            for child_node in node.get_children():
+                for ref in child_node.references:
+                    contents.append(ref)
+            jswindow["contents"] = contents
+            #
+            cls.jswindows.append(jswindow)
+        print(json.dumps(cls.jswindows))
+        return json.dumps(cls.jswindows)
+
+    @classmethod
+    def generate_nav_tree(cls):
+        """Here is generated tree-like structure of menu items, used by Navigation
+        class to present it's menu items in a pretty way."""
+        pass
+
     @classmethod
     def print_html(cls):
         # todo this
