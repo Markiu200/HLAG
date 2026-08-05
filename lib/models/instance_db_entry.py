34839a19b5df9cefde196e3aa449c2d00515b4e7
markiu200 <markiu2007@interia.pl>
2026-08-05 19:17:00 +0200

    Remove unused code in InstanceDBEntry class

--- lib/models/instance_db_entry.py
+++ lib/models/instance_db_entry.py
@@ -3,8 +3,3 @@ class InstanceDBEntry:
         self.module = module
         self.data = data
         self.meta = meta
-
-
-if __name__ == "__main__":
-    a = InstanceDBEntry("module", 12, 45)
-    print(a.module, a.data, a.meta)
