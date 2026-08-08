8af03868c6d6d83b410d269a5363d7bbfc4d6d92
markiu200 <markiu2007@interia.pl>
2026-08-08 03:33:44 +0200

    Implement as_string method of Ref class
    It simply returns Ref as string.

--- lib/models/dn_ref.py
+++ lib/models/dn_ref.py
@@ -2,3 +2,6 @@ class Ref:
     def __init__(self, module: str, ref_id: int):
         self.module = module
         self.ref_id = ref_id
+
+    def as_string(self) -> str:
+        return f"[%JSREF({self.module},{self.ref_id})%]"
