0b47a1f378be2fa775fefa26359090c7ee48c5e7
markiu200 <markiu2007@interia.pl>
2026-08-08 14:42:53 +0200

    Implement ability to register "delayed" prints in JSManager
    Delayed prints will be appended to the end of normal prints - useful if you need some code that always has to be at the end.

--- lib/js_manager/js_manager.py
+++ lib/js_manager/js_manager.py
@@ -3,6 +3,11 @@ from pathlib import PurePath
 
 class JSManager:
     registered_prints = []
+    registered_delayed_prints = []
+
+    #
+    # In order prints
+    #
 
     @classmethod
     def append_print(cls, resource_type: str, resource):
@@ -19,9 +24,32 @@ class JSManager:
     def register_print(cls, callback):
         cls.append_print("callback", callback)
 
+    #
+    # Delayed prints
+    #
+    @classmethod
+    def append_print_delayed(cls, resource_type: str, resource):
+        cls.registered_delayed_prints.append({
+            "resource_type": resource_type,
+            "resource": resource
+        })
+
+    @classmethod
+    def register_file_delayed(cls, path: PurePath):
+        cls.append_print_delayed("file", path)
+
+    @classmethod
+    def register_print_delayed(cls, callback):
+        cls.append_print_delayed("callback", callback)
+
+    #
+    # Printing methods
+    #
+
     @classmethod
     def print(cls):
         yield f"<script>\n"
+        cls.registered_prints.extend(cls.registered_delayed_prints)
         for element in cls.registered_prints:
             if element["resource_type"] == "file":
                 with open(element["resource"]) as f:
